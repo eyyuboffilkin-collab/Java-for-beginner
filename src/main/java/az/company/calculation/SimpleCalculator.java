2aaf1b86af84fd5eeb8130c711443078ce7dd700
Ilkin Eyyubov <eyyuboffilkin@example.com>
2026-03-20 13:03:59 +0400

    SimpleCalculator

---
+++ src/main/java/az/company/calculation/SimpleCalculator.java
@@ -0,0 +1,15 @@
+package az.company.person;
+
+public class SimpleCalculator {
+    static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("a=" +  a + ", b=" + b);
+
+    }
+
+}
